0cfd47674b03b4995013a445c54d472ccab11914
Bipana-Rai <bipanarai982@gmail.com>
2025-06-02 19:56:21 +0545

    d

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ const path = require("path");
 const app = express();
 const PORT = 5000;
 
-app.use(cors());
-app.use(cors());
+app.use(cors({
+  origin: '*', // allow all origins
+  credentials: true, // you can remove if not sending credentials
+}));
+
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
